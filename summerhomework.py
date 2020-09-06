1223f5bc7e9c1f248af84b920ace591bce349c5c
mol-george_gm-mbp <mol.george@gmail.com>
2020-09-06 23:49:45 +0100

    updated get_user_input function

--- summerhomework.py
+++ summerhomework.py
@@ -1,23 +1,25 @@
 trials = 2
-rounds = 1
-message1 = f"Please enter a number from 1 - {len(fruits)} to get a fruit: "
-message2 = f"You did not enter a number. Try again!\n"
-message3 = f"You number is not from 1 - {len(fruits)}. Try again!\n"
+round = 1
 fruits = ["apple", "grape", "orange", "peach", "pear", "banana"]
+m1 = f"\nYou can pick {trials} fruits."
+m2 = f"You did not enter a number. Try again!\n"
+m3 = f"You number is not from 1 - {len(fruits)}. Try again!\n"
 
-def get_user_input():
+def get_user_input(m):
 	while True:
 		try:
-			fruit_number = int(input(message1))
+			fruit_number = int(input(m))
 		except ValueError:
-			print(message2)
+			print(m2)
 		else:
-			if fruit_number < 0 or fruit_number > len(fruits):
-				print(message3)
+			if fruit_number < 1 or fruit_number > len(fruits):
+				print(m3)
 			else:
 				return fruit_number
 
-while trials >= rounds:
-	fruits.sort()
-	print(fruits[get_user_input() - 1], "\n")
-	rounds += 1
+fruits.sort()
+print(m1)
+while trials >= round:
+	m = f"\nEnter a number from 1 - {len(fruits)} to pick fruit #{round}: "
+	print(fruits[get_user_input(m) - 1])
+	round += 1

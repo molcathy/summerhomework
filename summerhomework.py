47bdd850ecd4df4f2a090e9561337fbc463dc52c
mol-george_gm-mbp <mol.george@gmail.com>
2020-09-06 19:15:18 +0100

    msc

--- summerhomework.py
+++ summerhomework.py
@@ -1,26 +1,25 @@
 fruits = ["apple", "pear", "orange", "grape", "peach"]
+trials = 2
+rounds = 1
 
-number_fruit = int(input("Please enter a number from 1 - 5 to get a fruit: "))
-
-trials = 1
-runs = 0
-
-while trials >= runs:
+while trials >= rounds:
+	number_fruit = int(input("Please enter a number from 1 - 5 to get a fruit: "))
 	if number_fruit == 1:
 		print(fruits[0])
-		runs += 1
+		rounds += 1
 	elif number_fruit == 2:
 		print(fruits[1])
-		runs += 1
+		rounds += 1
 	elif number_fruit == 3:
 		print(fruits[2])
-		runs += 1
+		rounds += 1
 	elif number_fruit == 4:
 		print(fruits[3])
-		runs += 1
+		rounds += 1
 	elif number_fruit == 5:
 		print(fruits[4])
-		runs += 1
+		rounds += 1
 	else:
 		print("You did not enter a number from 1-5, you can try again")
-		runs += 1
+		rounds += 1
+
